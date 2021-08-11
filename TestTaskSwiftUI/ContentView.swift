3636db321cf9a14992d250fdee1ebf3caeb1416e
alex-malygin <>
2021-08-11 17:08:58 +0300

    Added play audio

--- TestTaskSwiftUI/ContentView.swift
+++ TestTaskSwiftUI/ContentView.swift
@@ -5,11 +5,49 @@
 //
 
 import SwiftUI
+import AVKit
 
 struct ContentView: View {
+    @State private var audioPlayer: AVAudioPlayer?
+    @State private var showingAlert = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Spacer()
+        Button(action: {},
+               label: {
+                Text("Play")
+                    .bold()
+                    .frame(width: 150, height: 50, alignment: .center)
+                    .foregroundColor(.white)
+                    .background(Color.blue)
+                    .cornerRadius(8)
+                    .onLongPressGesture(minimumDuration: .infinity, maximumDistance: .infinity, pressing: { pressing in
+                        
+                        if pressing {
+                            audioPlayer?.play()
+                        } else {
+                            audioPlayer?.pause()
+                        }
+                    }, perform: {})
+               })
+            .onAppear {
+                initPlayer()
+            }
+        Spacer()
+    }
+    
+    private func initPlayer() {
+        // Setup local audio path
+        guard let soundPath = Bundle.main.path(forResource: "dtmf_1", ofType: "mp3") else { return }
+        let url = URL(fileURLWithPath: soundPath)
+        
+        // Set audioPlayer
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: url)
+        } catch {
+            debugPrint("Failed to play file")
+        }
+        
     }
 }
 
