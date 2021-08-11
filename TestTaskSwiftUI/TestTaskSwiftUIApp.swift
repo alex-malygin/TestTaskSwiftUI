0f939480de5673575de02bca1b493111d5d496ea
alex-malygin <>
2021-08-11 16:00:10 +0300

    Initial commit

---
+++ TestTaskSwiftUI/TestTaskSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestTaskSwiftUIApp.swift
+//  TestTaskSwiftUI
+//
+//  Created by Eugene Shapovalov on 8/11/21.
+//
+
+import SwiftUI
+
+@main
+struct TestTaskSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
